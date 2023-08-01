611acb83d952ac96d72885a5d6f1c3ff4b1c3153
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-08-01 05:42:35 +0300

    ref: Обработка неправильного пути

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const userRouters = require('./routes/users');
 const cardRouters = require('./routes/cards');
 
+const NOT_FOUND_ERROR_CODE = 404;
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
@@ -17,6 +19,10 @@ app.use((req, res, next) => {
 });
 app.use('/', cardRouters);
 app.use('/', userRouters);
+app.use((req, res, next) => {
+  res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Страница не найдена' });
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
