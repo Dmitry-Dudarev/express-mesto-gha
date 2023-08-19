2be239cb495b6cd02b408056dca7134abc59d2bd
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-08-20 02:06:53 +0300

    ref: нестыковки ф и б токен

--- controllers/users.js
+++ controllers/users.js
@@ -129,10 +129,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'secret-key', { expiresIn: '7d' });
-      res.send({
-        token,
-        email,
-      });
+      res.send({ token });
     })
     .catch(next);
 };
