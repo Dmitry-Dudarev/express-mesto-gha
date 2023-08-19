6fdfa3e2f4b7e547f8e672b850266b63bfd401c3
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-08-19 23:53:01 +0300

    ref: удален старый код.

--- app.js
+++ app.js
@@ -2,17 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const { celebrate, Joi, errors } = require('celebrate');
-
-// const { login, createUser } = require('./controllers/users');
-// const userRouters = require('./routes/users');
-// const cardRouters = require('./routes/cards');
-// const auth = require('./middlewares/auth');
 
 const complexRouters = require('./routes/index');
 
 const cors = require('./middlewares/cors');
-// const NotFoundError = require('./errors/not-found-error');
 
 const { PORT = 3000 } = process.env;
 
@@ -26,45 +19,6 @@ app.use(bodyParser.json());
 app.use(cors);
 
 app.use(complexRouters);
-// app.post('/signin', celebrate({
-//   body: Joi.object().keys({
-//     email: Joi.string().email().required(),
-//     password: Joi.string().required(),
-//   }),
-// }), login);
-
-// app.post('/signup', celebrate({
-//   body: Joi.object().keys({
-//     name: Joi.string().min(2).max(30),
-//     about: Joi.string().min(2).max(30),
-//     avatar: Joi.string().pattern(
-// /^https?:\/\/(www\.)?[a-z0-9\-._~:/?#[\]@!$&'()*+,;=]+\.{1}[a-z0-9\-._~:/?#[\]@!$&'()*+,;=]+#?$/i
-// ),
-//     email: Joi.string().email().required(),
-//     password: Joi.string().required(),
-//   }),
-// }), createUser);
-
-// app.use(auth);
-// app.use('/', cardRouters);
-// app.use('/', userRouters);
-
-// app.use((req, res, next) => {
-//   const error = new NotFoundError('Страница не найдена');
-//   next(error);
-// });
-
-// app.use(errors());
-
-// app.use((err, req, res, next) => {
-//   const { statusCode = 500, message } = err;
-//   res.status(statusCode).send({
-//     message: statusCode === 500
-//       ? 'На сервере произошла ошибка'
-//       : message,
-//   });
-//   next();
-// });
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
