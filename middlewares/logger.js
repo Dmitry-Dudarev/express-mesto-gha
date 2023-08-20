c8f1345aa2d41ca172a47a5f219e1c5227c08b9f
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-08-20 16:02:56 +0300

    feat: логирование

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -3,14 +3,14 @@ const expressWinston = require('express-winston');
 
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: '../request.log' }),
+    new winston.transports.File({ filename: '../request.log', level: 'debug' }),
   ],
   format: winston.format.json(),
 });
 
 const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.File({ filename: '../error.log' }),
+    new winston.transports.File({ filename: '../error.log', level: 'warn' }),
   ],
   format: winston.format.json(),
 });
