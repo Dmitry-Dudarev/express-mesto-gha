5d65440ba7b2235d280dac010775b7d5596b6f4e
Dmitry Dudarev <dr.nt2017@yandex.ru>
2023-08-20 14:18:56 +0300

    ref: удаление карточки

--- controllers/cards.js
+++ controllers/cards.js
@@ -58,7 +58,7 @@ module.exports.deleteCard = (req, res, next) => {
         throw new OwnershipError('Вы не можете удалять карточки, созданные другими пользователями');
       }
       Card.findByIdAndRemove(req.params.cardId)
-        .then(() => res.status(200).send('Пост удален'))
+        .then(() => res.status(200).send({ message: 'Пост удален' }))
         .catch(next);
     })
     .catch(next);
